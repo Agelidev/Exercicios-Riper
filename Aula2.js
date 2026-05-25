395eea533851a8c15ac7201a924acb50fc7d45c1
Agelidev <angelcarfer@gmail.com>
2026-05-25 20:41:05 -0300

    Aula2

---
+++ Aula2.js
@@ -0,0 +1,24 @@
+// 1. Crie duas variáveis numéricas e exiba a soma entre elas. 
+let a = 30;
+let b = 20;
+console.log (a + b);
+
+// 2. Verifique se uma variável idade é maior ou igual a 18. 
+let Ana = 14;
+let MaiorDeIdade = Ana >= 18; 
+console.log(MaiorDeIdade);
+
+//if (MaiorDeIdade == true) 
+//    {
+//        console.log("Ana é maior de idade!"); 
+//    }
+//else 
+//    {
+//        console.log("Ana não é maior de idade!")
+//    } 
+//Depois percebi q não precisavakk
+
+// 3. Combine duas expressões usando && e exiba o resultado. 
+let caixas = 23; 
+let verificarCaixas = caixas < 80  && caixas >= 20; 
+console.log("Temos um bom número de caixas: " + verificarCaixas + "Temos: " + caixas); 
