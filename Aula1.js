ddef7ae662e7a1196b0fc6f7ad2a6bcbdfbef152
Agelidev <angelcarfer@gmail.com>
2026-05-25 20:14:34 -0300

    Aula1

---
+++ Aula1.js
@@ -0,0 +1,13 @@
+// 1. Crie uma variável chamada 'produto' com o nome de um item de mercado.
+let produto = "Pera";
+
+// 2. Crie outra variável 'preco' com o valor numérico do item.
+let preco = 2.50;
+
+// 3. Use console.log para imprimir a seguinte frase: "O produto [produto] custa R$[preco]"
+console.log("O produto", produto, "custa R$", preco);
+
+// 4. Altere o valor de 'preco' e imprima novamente.
+console.log(preco);
+preco = 3;
+console.log("Novo preço:", preco);
