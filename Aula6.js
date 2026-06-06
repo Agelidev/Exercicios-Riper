e6e24ef1cbce1136c69da07816f54cfe54dbe6e8
Agelidev <angelcarfer@gmail.com>
2026-06-05 23:59:46 -0300

    Até aula 8

---
+++ Aula6.js
@@ -0,0 +1,18 @@
+//1. Crie um array com 3 cores e adicione uma nova cor.
+let cores = ["jade", "ferrugem", "púrpura"];
+cores.push ("carmesin");
+console.log(cores);
+
+//2. Remova o primeiro item do array usando shift.
+let cores2 = ["jade", "ferrugem", "púrpura"];
+cores2.shift ();
+console.log(cores2);
+
+//Aula
+//fruits.shift (); //tira do início
+//fruits.unshift(); //add no início
+//fruits.pop(); //tira do final
+//fruits.push(); //add no final
+
+//console.log(fruits.length); //mostra o tamanho da array
+//console.log(fruits.indexOf ("banana")); //mostra onde está/o index da informação pedida
---
+++ Aula7.js
@@ -0,0 +1,9 @@
+// 1. Crie um objeto chamado aluno com propriedades nome, idade e curso.
+let aluno = {nome: 'Gabriel', idade: 18, curso: 'Mecatrônica'};
+console.log(aluno);
+// 2. Altere o curso do aluno e exiba o objeto atualizado.
+aluno.curso ='Engenharia';
+console.log(aluno);
+// 3. Adicione uma nova propriedade chamada 'matriculado' com valor booleano.
+aluno.matriculado = true;
+console.log(aluno);
